19ff5a315204a27fe738e638761bcf80d4923bed
Arslon017 <101270450+Arslon017@users.noreply.github.com>
2022-06-01 19:45:41 +0500

    Add files via upload

---
+++ JSWORD_1/app.js
@@ -0,0 +1,81 @@
+// 1- masala
+
+// 
+// function hisobla(){
+// 
+    // let uch = document.getElementById("uch").value 
+    // let uchochko = parseInt(uch)
+    // let ikki = document.getElementById("ikki").value 
+    // let ikkiochko = parseInt(ikki)
+// 
+    // let hisob = (uchochko * 3) + (ikkiochko * 2)
+    // document.getElementById("natija").textContent = "Natija: " + hisob
+// }
+
+// ==================================
+
+// 2-masala
+// 
+// function buyumlar(){
+// 
+    // let mahsulotNomi = document.getElementById("mahsulot").value 
+    // let mahsulotNarxi = parseInt(document.getElementById("narx").value)
+    // let mahsulotMiqdiri = parseInt(document.getElementById("miqdor").value)
+// 
+    // 
+    // let buyum = document.getElementById("buyum").textContent =mahsulotNomi
+// 
+    // let narxlari = parseInt(document.getElementById("narxi").textContent = mahsulotNarxi + " so'm") 
+// 
+    // let miqdorlari = parseInt(document.getElementById("miqdori").textContent = mahsulotMiqdiri + " ta") 
+// 
+// 
+    // let umum = narxlari * miqdorlari
+// 
+    // let his = document.getElementById("umumiynarxi").textContent = umum + " so'm"
+// 
+    // 
+// }
+
+// =======================
+
+// 3-masala
+// 
+// function imtihon(){
+// 
+    // let fan = document.getElementById("fan").value 
+    // let otganlar = parseInt(document.getElementById("otganlar").value)
+    // let otolmaganlar = parseInt(document.getElementById("otolmaganlar").value)
+    // let kelmaganlar = parseInt(document.getElementById("kelmaganlar").value)
+// 
+    // let fan1 = document.getElementById("fan1").textContent = fan
+// 
+    // // let otganlar1 = document.getElementById("otganlar1").textContent = (otganlar * 100) / (otganlar + otolmaganlar + kelmaganlar) + " %"
+// 
+    // // let otolmaganlar1 = document.getElementById("otolmaganlar1").textContent = (otolmaganlar * 100) / (otganlar + otolmaganlar + kelmaganlar) + " %"
+// 
+    // // let kelmaganlar1 = document.getElementById("kelmaganlar1").textContent = (kelmaganlar * 100) / (otganlar + otolmaganlar + kelmaganlar) + " %"
+// }
+
+// =====================================
+
+// 4-masala
+
+// function Aylana(){
+// 
+    // let aylana = parseInt(document.getElementById("aylana").value) 
+    // let  π = 3.14
+// 
+    // document.getElementById('natija').textContent = aylana * 2 *   π 
+// }
+
+// =======================================
+
+// 5-masala
+
+function harorat(){
+
+    let selsi = parseInt(document.getElementById("selsi").value)
+
+    document.getElementById("natija").textContent = selsi * 1.8 + 32
+}
